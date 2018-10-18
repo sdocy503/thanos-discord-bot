709c7d5639a40d12148a812d7222631d86be1dd8
sdocy503 <docystephen@gmail.com>
2018-10-17 18:40:17 -0700

    Thanos bot working

---
+++ RiskBot - Copy/RiskBot/Modulues/ping.cs
@@ -0,0 +1,102 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Addons.Interactive;
+using System.Threading;
+
+namespace RiskBot.Modulues
+{
+    public class mainThanos : InteractiveBase
+    {
+        Random rnd = new Random();
+        [Command("snap", RunMode = RunMode.Async)]
+        public async Task snapTime()
+        {
+            var senderId = Context.User.Id;
+            var sender = Context.Guild.GetUser(senderId);
+            if(!sender.GuildPermissions.Administrator && !sender.GuildPermissions.BanMembers)
+            {
+                await ReplyAsync("You do not have permission to snap.");
+            }
+            else
+            {
+                List<ulong> allUsersId = new List<ulong>();
+                List<string> allUsersName = new List<string>();
+
+                //Gets all users
+                var users = Context.Guild.Users;
+                foreach (var user in users)
+                {
+                    allUsersId.Add(user.Id);
+                    allUsersName.Add(user.Mention);
+                    try
+                    {
+                        await Discord.UserExtensions.SendMessageAsync(user, $"Rejoice, for Thanos has arrived to balance the discord server: {Context.Guild.Name}.  If you are one of the many who were sacrificed know that your sacrifice will help maintain the stability of the server.  But there is a chance you can come back, if you do want to come back try contacting {Context.User.Username}.");
+                    }
+                    catch
+                    {
+
+                    }
+                    //await ReplyAsync(user.Id.ToString());
+                }
+
+                //Randomizes users
+                int n = allUsersId.Count;
+                while (n > 1)
+                {
+                    n--;
+                    int k = rnd.Next(n + 1);
+                    ulong value = allUsersId[k];
+                    string valueName = allUsersName[k];
+                    allUsersId[k] = allUsersId[n];
+                    allUsersName[k] = allUsersName[n];
+                    allUsersId[n] = value;
+                    allUsersName[n] = valueName;
+                }
+
+                //Print out the randomized list
+                /*for (int i = 0; i < allUsers.Count; i++)
+                {
+                    await ReplyAsync(allUsers[i].ToString());
+                }*/
+
+                double userCount = allUsersId.Count;
+                userCount = userCount / 2;
+
+                int placeCounter = 0;
+                int killCounter = 0;
+                bool success = true;
+                while (killCounter < userCount)
+                {
+                    if (placeCounter > allUsersId.Count)
+                    {
+                        await ReplyAsync("Thanos can't ban enough people to balance the server, he has tried his best though.");
+                        success = false;
+                        break;
+                    }
+                    try
+                    {
+                        await Context.Guild.AddBanAsync(allUsersId[placeCounter], 0);
+                        await ReplyAsync($"{allUsersName[placeCounter]} was snapped by Thanos for the greater good of the server.");
+                        killCounter++;
+                        
+                    }
+                    catch(Exception e)
+                    {
+                        //Console.Write(e);
+                    }
+                    placeCounter++;
+                }
+
+                if (success)
+                {
+                    await ReplyAsync("Thanos has balanced your server.");
+                }
+                //await ReplyAsync();
+            }
+        }
+    }
+
+}
---
+++ RiskBot - Copy/RiskBot/Program.cs
@@ -0,0 +1,80 @@
+﻿using Discord;
+using Discord.Addons.Interactive;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace RiskBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .AddSingleton<InteractiveService>()
+                .BuildServiceProvider();
+
+            string botToken = "NTAwODU3NDIxNzUzNzQ1NDA4.Dqagrw.WAb4dThn-VxgfHIFnvJ1zydBc4I";
+
+            //event subscriptions
+            _client.Log += Log;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, botToken);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        private async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            if(message is null || message.Author.IsBot) return;
+
+            int argPos = 0;
+
+            if(message.HasStringPrefix("!", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos)){
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+        }
+    }
+}
